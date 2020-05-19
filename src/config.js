a25a0ceb08431f8c7a304191e5fabc7ebfdda9bf
jackyc58 <jackyc58@hotmail.com>
2020-05-19 17:42:08 +0800

    Setting up our Rect app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "chh-bucket"
+  },
+  apiGateway: {
+    REGION: "ap-northeast-1",
+    URL: "https://tn4n3irpl7.execute-api.ap-northeast-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-northeast-1",
+    USER_POOL_ID: "ap-northeast-1_xomK8RX7r",
+    APP_CLIENT_ID: "5skoumflgpi6sr3j247orn47mr",
+    IDENTITY_POOL_ID: "ap-northeast-1:5f5a80cc-ca9b-4828-b892-03df32bdd7f7"
+  }
+};
